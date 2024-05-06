9ecfba8adb7c091840e8d6eff65bb241a3046708
LimBell <nowwhd987@naver.com>
2024-05-06 15:34:53 +0900

    트위터 탐색 중 메모가 정상적이지 아니하게 표시되거나, 표시되지 않는 버그 수정

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,6 @@
 const idPosition = 'head > script:nth-child(49)';
 const memoPosition = '#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div.css-175oi2r.r-6gpygo.r-14gqq1x > div.css-175oi2r.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-175oi2r.r-1awozwy.r-18u37iz.r-1wbh5a2 > div > div > div > span';
+const observePosition = '#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div.css-175oi2r.r-6gpygo.r-14gqq1x > div.css-175oi2r.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div';
 function getId() {
     var a = document.querySelectorAll(idPosition)
     var b = Array.from(a)
@@ -12,11 +13,12 @@ function getId() {
         return 'not found';
     }
 }
-
-regex_url = /^https:\/\/twitter\.com\/(?!home$).+/;
+  
+//regex_url = /^https:\/\/twitter\.com\/(?!home$).+/;
+regex_twitter_handle = /^https:\/\/twitter\.com\/(?!home$)([a-zA-Z0-9_]{1,15})$/;
 
 var id = 'not found';
-const observer = new MutationObserver(async (mutationList, observer) => {
+const observer_initial = new MutationObserver(async (mutationList, observer) => {
     id = getId();
     if (id !== 'not found'){
         console.log('found id :' + id.identifier);
@@ -24,18 +26,45 @@ const observer = new MutationObserver(async (mutationList, observer) => {
         await chrome.runtime.sendMessage({action: 'getMemo', id: id.identifier}, function(response) {
             memo = response.res;
             if (memo !== ''){
-                console.log('memo is printed on document' + memo);
-                Array.from(document.querySelectorAll(memoPosition))[0].textContent += ' : \'' + memo + '\'';
+                var target_handle=Array.from(document.querySelectorAll(memoPosition))[0]
+                var handle = window.location.href.match(regex_twitter_handle)[1];
+                target_handle.textContent = '@' + handle + ' : \'' + memo + '\'';
             }
         });
     }
 });
+
 const el = document.getElementsByTagName('head');
-observer.observe(el[0], {
+window.addEventListener('popstate', function() {
+    console.log('popstate changed');
+    observer_initial.observe(el[0], {
+        childList: true,
+        subtree: true,
+        attributeOldValue: true,
+    });
+});
+observer_initial.observe(el[0], {
     childList: true,
     subtree: true,
     attributeOldValue: true,
 });
+
+const observeUrlChange = () => {
+    let oldHref = document.location.href;
+    const body = document.querySelector("body");
+    const observer = new MutationObserver(mutations => {
+        if (oldHref !== document.location.href) {
+            oldHref = document.location.href;
+            observer_initial.observe(el[0], {
+                childList: true,
+                subtree: true,
+                attributeOldValue: true,
+            });
+        }
+    });
+    observer.observe(body, { childList: true, subtree: true });
+};
+window.onload = observeUrlChange;
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
